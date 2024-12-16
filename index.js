4de2284c5d70f348598f1b4f87373099b6446aa3
shoaibkhan73 <khan973973@gmail.com>
2024-12-17 00:44:36 +0500

    practice no 9 is comleted

---
+++ index.js
@@ -0,0 +1,51 @@
+console.log("Practice no 9 js Number and math  ");
+
+
+
+let a = 4544;
+let c = new Number(543456344);
+
+// console.log(a);
+// console.log(c.toString().length);
+// console.log(c.toFixed(2));
+// console.log(c.toLocaleString(`en-PK`));
+
+
+
+let d = 56545.345;
+// console.log(d.toPrecision(7));
+
+let e = 1435412;
+// console.log(e.toLocaleString());
+
+
+
+
+
+
+
+// +++++++++++++++++   Maths   +++++++++++        
+
+
+
+/*
+console.log(Math.abs(-4))
+console.log(Math.round(4.5));
+console.log(Math.floor(4.5));
+console.log(Math.ceil(5.54));
+console.log(Math.min(4 , 5 , 7 , 1 ));
+console.log(Math.max(5 , 4 , 1 , 7 , 24 , 9 ));
+*/
+
+
+console.log(Math.random());
+console.log((Math.random() * 10));
+console.log((Math.random() * 10) + 4);
+console.log(Math.floor(Math.random() * 10) + 4);
+
+
+
+let f = 10;
+let g = 20;
+
+console.log(Math.random() * (g - f + 1) + f);
